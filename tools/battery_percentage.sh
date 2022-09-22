1a00ec52d6d2f96fa1480bbecedd4a264d5acd9a
ishtar <ishtar@oopsec.dev>
2022-09-22 09:28:05 +0300

    2022-09-22

--- tools/battery_percentage.sh
+++ tools/battery_percentage.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash -
 
-BAT=/sys/class/power_supply/BAT1
-CURR=$(cat $BAT/charge_now)
-FULL=$(cat $BAT/charge_full)
-PERC=$(echo "100.0 * $CURR / $FULL" | bc -l)
+BAT=/sys/class/power_supply/BAT0
+CURR=$(cat $BAT/energy_now)
+FULL=$(cat $BAT/energy_full)
+PERC=$((100 * $CURR / $FULL))
 
-printf " %.2f\n" "$PERC"
+printf " %2.2f\n" "$PERC"
 
 STAT=$(cat /sys/class/power_supply/BAT1/status)
 CAP=$(cat /sys/class/power_supply/BAT1/capacity)
